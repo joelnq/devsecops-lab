d0d82ad69b1787f229ebeee60b11772a4f0aa43a
joelnq <nicharjoelquispealarcon@gmail.com>
2026-08-15 11:36:30 -0400

    fix: excluir directorio .github del escaneo de semgrep

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_s3_bucket" "bucket_seguro" {
   bucket = "mi-bucket-devsecops-demo-12345"
 }
-# CORRECCIÓN IaC: Bloqueo explícito de acceso público
+
 resource "aws_s3_bucket_public_access_block" "publico" {
   bucket                  = aws_s3_bucket.bucket_seguro.id
   block_public_acls       = true
@@ -12,13 +13,16 @@ resource "aws_s3_bucket_public_access_block" "publico" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
 resource "aws_security_group" "sg_seguro" {
-  name = "sg_ssh_restringido"
+  name        = "sg_ssh_restringido"
+  description = "Grupo de seguridad restringido para acceso SSH privado"
+
   ingress {
+    description = "Acceso SSH unicamente desde la red interna"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    # CORRECCIÓN IaC: Acceso SSH restringido a red privada
     cidr_blocks = ["10.0.0.0/16"]
   }
 }
